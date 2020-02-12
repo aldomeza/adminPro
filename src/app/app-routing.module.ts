763405bafa9bce091e73995253b16efa42828385
Aldo Meza <aldomezaj@gmail.com>
2020-02-12 18:49:39 -0300

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Página Principal
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 
-const routes: Routes = [];
+// Rutas hijas
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficaComponent } from './pages/grafica/grafica.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
+
+const routes: Routes = [
+  {path: '', component: PagesComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'Graficas1', component: GraficaComponent},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    ]},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegisterComponent},
+  {path: '**', component: NopagefoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

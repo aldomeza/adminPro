d501685e2db1af91f71e3ce71a8c5eca48aad3b0
Aldo Meza <aldomezaj@gmail.com>
2020-02-13 06:57:40 -0300

    Modificaciones en los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Página Principal
-import { PagesComponent } from './pages/pages.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
-// Rutas hijas
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficaComponent } from './pages/grafica/grafica.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
-  {path: '', component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'Graficas1', component: GraficaComponent},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]},
   {path: 'login', component: LoginComponent},
   {path: 'registro', component: RegisterComponent},
   {path: '**', component: NopagefoundComponent}
